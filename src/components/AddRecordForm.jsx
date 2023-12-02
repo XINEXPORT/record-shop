2ad9f19c003167ac5ce750d3e68dab1e99b4d507
Christine Hoang <christinehoangt@gmail.com>
2023-12-02 14:03:24 -0600

    addrecordform onchange included

--- src/components/AddRecordForm.jsx
+++ src/components/AddRecordForm.jsx
@@ -2,17 +2,21 @@ import { useState } from 'react'
 import { useEffect } from 'react';
 
 const AddRecordForm=()=>{
-const [artist_name, setsetartist_name] = useState('');
-const [album_name, setsetalbum_name] = useState('');
-const [album_price, setsetalbum_price] = useState('');
+const [artist_name, setartist_name] = useState('');
+const [album_name, setalbum_name] = useState('');
+const [album_price, setalbum_price] = useState('');
+const [album_img, setalbum_img] = useState('');
 
 const handleArtistNameChange=(e)=> setartist_name(e.target.value);
 const handleAlbumNameChange=(e)=> setalbum_name(e.target.value);
 const handleAlbumPriceChange=(e)=> setalbum_price(e.target.value);
-
+const handleAlbumImg=(e)=> setalbum_img(e.target.value);
 
 return (
 <div className="record-form">
+    <label htmlFor="album-img">Album IMG URL</label>
+    <input id ="album-img" type="text" value={album_img} onChange={handleAlbumImg}/>
+
     <label htmlFor="artist-name">Artist Name</label>
     <input id="artist-name" type="text" value={artist_name} onChange={handleArtistNameChange}/>
 
