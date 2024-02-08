3fc1ff041e02f1d53a2549b7819528c38e913080
Christine Hoang <christinehoangt@gmail.com>
2024-02-08 09:56:17 -0600

    saving

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import{
     updateRecord,
     deleteRecord
 } from './controllers/controller.js'
-
+import path from 'path'
 const app = express()
 const port = '8000'
 
@@ -19,6 +19,9 @@ app.use(express.static("index.html"))
 ViteExpress.config({ printViteDevServerHost: true });
 
 // Routes go here
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
 
 app.get('/api/record', getRecords)
 app.post('/api/record', addRecord)
